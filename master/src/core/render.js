6f8a211615d5089f3bb5ad2aeb80c6063576a83b
joycezhang <787027175@qq.com>
2020-06-02 09:42:38 +0800

    修改 app 路径

--- master/src/core/render.js
+++ master/src/core/render.js
@@ -2,7 +2,7 @@ import Vue from "vue"
 import router from "../router"
 import store from "../store"
 
-import App from "../App.vue"
+import App from "../app/App.vue"
 
 export function vueRender() {
     Vue.config.productionTip = false
