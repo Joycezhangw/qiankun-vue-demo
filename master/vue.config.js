c809f114b2bbfaef319bef4bac8b1ab720cc43c6
joycezhang <787027175@qq.com>
2020-06-03 11:50:53 +0800

    处理 svg-sprite-loader 和 url-loader 冲突

--- master/vue.config.js
+++ master/vue.config.js
@@ -1,4 +1,9 @@
+'use strict'
+const path = require('path')
 const { port } = require("./package");
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
 
 module.exports = {
     // publicPath: './',
@@ -16,6 +21,25 @@ module.exports = {
         headers: {
             'Access-Control-Allow-Origin': '*',
         }
+    },
+    chainWebpack(config) {
+        //vue-cli默认情况下会使用 url-loader 对svg进行处理，会将它放在/img 目录下，所以这时候我们引入svg-sprite-loader 会引发一些冲突。
+        //使用 webpack 的 exclude 和 include ，让svg-sprite-loader只处理你指定文件夹下面的 svg，这样就避免了冲突，页面无法引用到
+        config.module
+            .rule('svg')
+            .exclude.add(resolve('src/icons'))
+            .end()
+        config.module
+            .rule('icons')
+            .test(/\.svg$/)
+            .include.add(resolve('src/icons'))
+            .end()
+            .use('svg-sprite-loader')
+            .loader('svg-sprite-loader')
+            .options({
+                symbolId: 'icon-[name]'
+            })
+            .end()
     },
     pages: {
         index: {
