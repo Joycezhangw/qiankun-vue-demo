a6f7836eaf09da40bfef1f7c2a75369530358b1b
joycezhang <787027175@qq.com>
2020-06-11 15:02:42 +0800

    将主入口文件移出来

--- master/src/main.js
+++ master/src/main.js
@@ -1,10 +1,10 @@
 /**
  * 统一引入的外部插件、样式、服务等
  */
-import '../core/install'
+import './core/install'
 //导入 svg 图标
-import '../icons'
-import {vueRender} from "../core/render"
+import './icons'
+import {vueRender} from "./core/render"
 
 
 vueRender({},true)
