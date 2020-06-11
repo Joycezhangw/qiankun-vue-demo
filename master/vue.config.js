01a86aee032d868dc767bb61f770e944ba0601d6
joycezhang <787027175@qq.com>
2020-06-11 15:04:51 +0800

    配置全局less变量以及相关webpack配置

--- master/vue.config.js
+++ master/vue.config.js
@@ -1,19 +1,34 @@
 'use strict'
 const path = require('path')
-const { port } = require("./package");
+const { publicPath,
+    assetsDir,
+    outputDir,
+    lintOnSave,
+    devPort,
+    title } = require("./src/config/settings")
+const { version, author } = require("./package.json");
+const time = require("dayjs")().format("YYYY-M-D HH:mm:ss");
+process.env.VUE_APP_TITLE = title || "himall";
+process.env.VUE_APP_AUTHOR = author;
+process.env.VUE_APP_UPDATE_TIME = time;
+process.env.VUE_APP_VERSION = version;
+
 function resolve(dir) {
     return path.join(__dirname, dir)
 }
 
 module.exports = {
-    // publicPath: './',
+    publicPath,
+    assetsDir,
+    outputDir,
+    lintOnSave,
     devServer: {
         /* 自动打开浏览器 */
         open: true,
         // host: '0.0.0.0',
         hot: true,
         disableHostCheck: true,
-        port,
+        port: devPort,
         overlay: {
             warnings: false,
             errors: true
@@ -23,6 +38,31 @@ module.exports = {
         }
     },
     chainWebpack(config) {
+        //分包
+        config.optimization.splitChunks({
+            chunks: "all",
+            cacheGroups: {
+                libs: {
+                    name: "chunk-libs",
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: 10,
+                    chunks: "initial",
+                },
+                elementUI: {
+                    name: "chunk-elementUI",
+                    priority: 20,
+                    test: /[\\/]node_modules[\\/]_?element-ui(.*)/,
+                },
+                commons: {
+                    name: "chunk-commons",
+                    test: resolve("src/components"),
+                    minChunks: 3,
+                    priority: 5,
+                    reuseExistingChunk: true,
+                },
+            },
+        });
+        config.optimization.runtimeChunk("single");
         //vue-cli默认情况下会使用 url-loader 对svg进行处理，会将它放在/img 目录下，所以这时候我们引入svg-sprite-loader 会引发一些冲突。
         //使用 webpack 的 exclude 和 include ，让svg-sprite-loader只处理你指定文件夹下面的 svg，这样就避免了冲突，页面无法引用到
         config.module
@@ -41,20 +81,15 @@ module.exports = {
             })
             .end()
     },
-    pages: {
-        index: {
-            // page 的入口
-            entry: 'src/app/main.js',
-            // 模板来源
-            template: 'public/index.html',
-            // 在 dist/index.html 的输出
-            filename: 'index.html',
-            // 当使用 title 选项时，
-            // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-            title: 'Hi Mall 微前端',
-            // 在这个页面中包含的块，默认情况下会包含
-            // 提取出来的通用 chunk 和 vendor chunk。
-            chunks: ['chunk-vendors', 'chunk-common', 'index']
+    pluginOptions: {
+        //配置全局css变量
+        "style-resources-loader": {
+            preProcessor: "less",
+            patterns: [
+                //这个是加上自己的路径，
+                //注意：试过不能使用别名路径
+                path.resolve(__dirname, 'src/styles/variables/*.less'),
+            ]
         }
     }
 }
