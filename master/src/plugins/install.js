6b5d52650347965e8714c3fbb2a640ac6cd65cfd
joycezhang <787027175@qq.com>
2020-06-11 15:11:34 +0800

    配置 饿了吗uisize

--- master/src/plugins/install.js
+++ master/src/plugins/install.js
@@ -6,11 +6,13 @@ import 'element-ui/lib/theme-chalk/index.css';
 
 //注册组件
 
-[...Element.components].forEach(i => Vue.use(i));
+[...Element.components].forEach(i => Vue.use(i, {
+    size: "small"
+}));
 
 // //注册指令服务
 
-// Element.serve.forEach(i => Vue.use(i.directive));
+Element.serve.forEach(i => Vue.use(i.directive));
 
 // //注册挂载方法
-// Element.methods.forEach(i => Vue.prototype[`$${i.name}`] = i);
+Element.methods.forEach(i => Vue.prototype[`$${i.name}`] = i);
