7fa9dda2f8b4dc3c6dd187eea69b3d8c7f4b40aa
joycezhang <787027175@qq.com>
2020-06-15 19:00:10 +0800

    如果不是子应用得话，隐藏掉子应用dom

--- master/src/layout/components/AppMain/index.vue
+++ master/src/layout/components/AppMain/index.vue
@@ -1,13 +1,15 @@
 <template>
   <main class="app-main-container">
+    <!-- 主应用渲染区，用于挂载主应用路由触发的组件 -->
     <transition name="fade-transform" mode="out-in">
       <!-- <keep-alive :include="cachedRoutes" :max="10"> -->
       <!-- <router-view :key="key" style="min-height: 80.6vh;" /> -->
       <!-- </keep-  alive> -->
       <router-view style="min-height:90.6vh" />
     </transition>
+    <!-- 子应用渲染区，用于挂载子应用节点 -->
     <transition name="fade-transform" mode="out-in">
-        <div id="appContainer" style="min-height:90.6vh"></div>
+      <div id="subapp-viewport" style="min-height:90.6vh" v-show="$route.meta.isMicrApp"></div>
     </transition>
     <footer class="footer-copyright">
       Copyright
